e458785835f9d61c51e9862df7cc6d966515f4c7
Gavin-Niederman <gavinniederman@gmail.com>
2024-06-21 20:54:31 -0700

    fix: pinned rust toolchain in flake

--- flake.nix
+++ flake.nix
@@ -4,16 +4,26 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, systems, ... }:
+  outputs = { nixpkgs, systems, rust-overlay, ... }:
     let eachSystem = nixpkgs.lib.genAttrs (import systems);
     in {
       devShells = eachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
+        let pkgs = import nixpkgs {
+          overlays = [ (import rust-overlay) ];
+          inherit system;
+        };
         in {
-          default =
-            pkgs.mkShell { buildInputs = with pkgs; [ pkg-config dbus udev ]; };
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rust-bin.nightly.latest.default
+              pkg-config
+              dbus
+              udev 
+            ];
+          };
         });
     };
 }
