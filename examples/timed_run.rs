df5475350a2cce8a31b7bf870634bdb435b9dded
Gavin-Niederman <gavinniederman@gmail.com>
2024-10-26 14:49:16 -0700

    feat: match mode packets

---
+++ examples/timed_run.rs
@@ -0,0 +1,84 @@
+use std::time::Duration;
+
+use log::{error, info};
+use tokio::time::sleep;
+use vex_v5_serial::{
+    connection::{
+        serial::{self, SerialError},
+        Connection,
+    },
+    packets::{
+        match_mode::{MatchMode, SetMatchModePacket, SetMatchModePayload, SetMatchModeReplyPacket},
+        system::{GetSystemVersionPacket, GetSystemVersionReplyPacket},
+    },
+};
+
+#[tokio::main]
+async fn main() -> Result<(), SerialError> {
+    simplelog::TermLogger::init(
+        log::LevelFilter::Info,
+        simplelog::Config::default(),
+        simplelog::TerminalMode::Mixed,
+        simplelog::ColorChoice::Always,
+    )
+    .unwrap();
+
+    // Find all vex devices on the serial ports
+    let devices = serial::find_devices()?;
+
+    // Open a connection to the device
+    let mut connection = devices[0].connect(Duration::from_secs(30))?;
+
+    let response = connection
+        .packet_handshake::<GetSystemVersionReplyPacket>(
+            Duration::from_millis(700),
+            5,
+            GetSystemVersionPacket::new(()),
+        )
+        .await?;
+
+    match response.payload.product_type {
+        vex_v5_serial::packets::system::ProductType::Brain => {
+            error!("You must be connected to the Brain over controller to use field control");
+            return Ok(());
+        }
+        vex_v5_serial::packets::system::ProductType::Controller => {}
+    }
+
+    info!("Setting match mode to auto");
+    connection.packet_handshake::<SetMatchModeReplyPacket>(
+        Duration::from_millis(500),
+        10,
+        SetMatchModePacket::new(SetMatchModePayload {
+            match_mode: MatchMode::Auto,
+            match_time: 0,
+        }),
+    ).await?;
+
+    sleep(Duration::from_secs(2)).await;
+
+    info!("Setting match mode to driver");
+    connection.packet_handshake::<SetMatchModeReplyPacket>(
+        Duration::from_millis(500),
+        10,
+        SetMatchModePacket::new(SetMatchModePayload {
+            match_mode: MatchMode::Driver,
+            match_time: 2,
+        }),
+    ).await?;
+
+    // 1 minute 45 seconds
+    sleep(Duration::from_secs(2)).await;
+
+    info!("Setting match mode to disabled");
+    connection.packet_handshake::<SetMatchModeReplyPacket>(
+        Duration::from_millis(500),
+        10,
+        SetMatchModePacket::new(SetMatchModePayload {
+            match_mode: MatchMode::Disabled,
+            match_time: 4,
+        }),
+    ).await?;
+
+    Ok(())
+}
--- src/packets/mod.rs
+++ src/packets/mod.rs
@@ -13,6 +13,7 @@ pub mod log;
 pub mod program;
 pub mod radio;
 pub mod system;
+pub mod match_mode;
 
 /// Header byte sequence used for all device-bound packets.
 pub const DEVICE_BOUND_HEADER: [u8; 4] = [0xC9, 0x36, 0xB8, 0x47];
